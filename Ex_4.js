39dde5ad8b855b5e032d9d63e868fe45f6b1e940
mikuhl <mikuhl@comcast.net>
2017-03-10 09:10:35 -0600

    homework

---
+++ Ex_4.js
@@ -0,0 +1,74 @@
+//Add Add two numbers together.
+var addNumbers = 8 + 15;
+console.log(addNumbers);
+
+//Add three numbers together using the addition operator.
+var addThreeNumbers = (8 + 15) + (-100);
+console.log(addThreeNumbers);
+
+//Add a positive and a negative number.
+var numberResult = (-5 + 32);
+console.log(numberResult);
+
+//Subtract two numbers.
+var minusNumber = 35 - 2;
+console.log(minusNumber);
+
+//Subtract three numbers.
+var minusNumber = 35 - 2.8 - 17;
+console.log(minusNumber);
+
+//Subtract a positive and a negative number.
+var minusNumber = (-5) + 32;
+console.log(minusNumber);
+
+// Combine the addition and subtraction operators with three numbers.
+var number = 75 + (-35) - (2.8);
+console.log(number);
+
+//Multiply two numbers
+var numberMult = 6 * (-0.1);
+console.log(numberMult);
+
+//Multiply three numbers where one is negative
+var numberMult = (-8) * 5 * (1.8);
+console.log(numberMult);
+
+//Divide two numbers
+var number = 5 / (-12);
+console.log(number);
+
+//Divide three numbers
+var numberDiv = 25 / -12 / 2;
+console.log(numberDiv);
+
+//Find the remainder of dividing two integers
+var numberInt = 25 % 4;
+console.log(numberInt);
+
+//An expression that uses each of the following: Negative and Positive Numbers, Integers and Doubles, Parenthesis, Addition, Subtraction, Multiplication, Division, and Modulo.
+var number = ((5 + (-4) - (-4) * 3) / 2.3)/ 9;
+console.log(number);
+
+//I will go out to work if I am employed and the job isnt taken, or there's Project I want.
+var employed = true;
+var jobTaken = false;
+var projectIWant = true;
+
+//* am I going to work? */
+console.log(employed && (jobTaken|| !projectIWant));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ answers.md
@@ -0,0 +1,23 @@
+week_4_lecture_2_exercises
+
+Select the string expressions from the listed options.
+A, B, C
+
+
+
+Select all the falsey values from the following options:
+A, B, C, E
+
+
+
+Define the term "strict equality".
+Strict Equality: compares two values for equality, checking value and type
+
+
+
+What does ((42 - 8) + 24 / 6 - 3 * 0) * (34 + (7 + (4 + 3) % 3) / 2) evaluate to? Please evaluate this by hand.
+A. 1444
+
+5. C. !
+
+
